29af04440ee96869dc63bd82d2d80517fdddda47
btd1337 <helder.bertoldo@ice.ufjf.br>
2018-01-19 23:13:40 -0200

    0.3
    - Improved script
    - Many icons missed revised

--- scripts/clean-install.sh
+++ scripts/clean-install.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-
+echo "Please, run this script in the /scripts folder!"
 sizes=(16 24 32 48 64 128 symbolic)
 directories=()
-home="$(pwd)"
+cd ../
+home="$(pwd)"   # into /scripts folder
+
 
 function main() {
     getDirectories
-
-    cd ../   
+    
     for directory in "${directories[@]}"
     do
         for size in "${sizes[@]}"
         do
             fileList $directory $size
-        done        
-    done
+       done        
+   done
     
 }
 
-function getDirectories() {
+function getDirectories() {                             # get all directories in the elementary icon folder
     local current=''
     cd /usr/share/icons/elementary 
     for directory in */
@@ -36,21 +37,25 @@ function getDirectories() {
 function fileList() {
     local directory=$1
     local size=$2
-    local search_dir=( $(find /usr/share/icons/elementary/$directory/$size/ -type f) )    
-    
-    for entry in "${search_dir[@]}"
-    do
-     removeIcon ${entry##*/} $directory $size         #${entry##*/} => it's necessary to not include the icon path
-     generateSymlink ${entry##*/} $directory $size
-    done
+    local search_file=( $(find /usr/share/icons/elementary/$directory/$size/) )    
+    if [ $search_file ]
+    then
+        for entry in "${search_file[@]}"
+        do  
+            if [ "${entry##*/}" != "" ]                          # block blank strings
+            then
+                removeIcon ${entry##*/} $directory $size         #${entry##*/} => it's necessary to not include the icon path
+                generateSymlink ${entry##*/} $directory $size           
+            fi
+        done
+    fi       
 }
 
 function removeIcon() {
     local icon=$1
     local directory=$2
     local size=$3
-
-    rm $directory/$size/$icon
+    rm -rf $directory/$size/$icon
 }
 
 function generateSymlink() {
@@ -58,8 +63,8 @@ function generateSymlink() {
     local directory=$2
     local size=$3
     cd $directory/$size/
-    ln -s /usr/share/icons/elementary/$directory/$size/$icon $icon
-    cd ../../
+    ln -sf /usr/share/icons/elementary/$directory/$size/$icon $icon
+    cd $home
 }
 
 main
